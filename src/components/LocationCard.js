fe24f2d542c30a801bee5cebb80ee1a05f089dc6
Brendan Schatz <brendanschatz97@gmail.com>
2019-10-18 13:08:16 -0400

    got the location working working with a search function

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+
+const Card = styled.div`
+background-color: green;
+width: 40%;
+display: flex;
+justify-content: center;
+flex-direction: column;
+align-items: center;
+margin: 1%;
+color: maroon;
+text-align: center;
+`
+
+export default function LocationCard(props) {
+  return (
+  <Card >
+    <div>
+      <h2>Name: {props.name}</h2>
+      <p>Type: {props.type}</p>
+      <p>Dimension: {props.dimension}</p>
+  </div>
+</Card>
+  )
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,6 +1,70 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import LocationCard from './LocationCard';
+import LocationSearch from './locationsearch';
+import styled from "styled-components";
+
+
+const List = styled.section`
+display:flex;
+flex-direction: column;
+align-items: center;
+width:100%;
+`
+
+
+
+
 
 export default function LocationsList() {
 
-return null
-}
+
+    const [location, setLocation] = useState([])
+    const [locSearch, setLocSearch] = useState("");
+    const [locPeram, setLocPeram] = useState('');
+
+    useEffect(() => {
+        axios
+            .get('https://rickandmortyapi.com/api/location/')
+            .then(response => {
+                setLocation(response.data.results);
+            })
+            .catch(error => {
+                console.log('error', error)
+            });
+        // TODO: Add API Request here - must run in `useEffect`
+        //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    }, []);
+    
+  const handleChange = event => {
+    setLocPeram(event.target.value.toLowerCase());
+  };
+
+const submit = e => {
+  e.preventDefault();
+  setLocSearch(locPeram)
+  setLocPeram("")
+  console.log(locPeram)
+};
+
+    return (
+        <List>
+
+        <LocationSearch
+          handleChange={handleChange}
+          locPeram={locPeram}
+          locSearch={locSearch}
+          submit={submit}
+        />
+        {location.map(loc => { if(loc.name.toLowerCase().includes(locSearch)){
+          return (
+          <LocationCard
+            name={loc.name}
+            type={loc.type}
+            dimension={loc.dimension}
+        />
+        )}})
+        }
+      </List>
+    );
+  }
