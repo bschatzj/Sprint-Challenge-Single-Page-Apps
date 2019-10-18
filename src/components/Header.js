bde672138855e12c6e20f2e4d025cafc282c6703
Brendan Schatz <brendanschatz97@gmail.com>
2019-10-18 11:22:30 -0400

    Got a basic router set up with a header working with links.  Added a base style to the header. Added react router as well as styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
+import styled from "styled-components";
+
+const Head = styled.header`
+display:flex;
+justify-content: space-evenly;
+`
+const Nav = styled.h3`
+font-size: 1.8rem;
+text-decoration: none;
+color: blue;
+`
+const Title = styled.div`
+color: red;
+text-align: center;
+font-size: 3rem;
+`
 
 export default function Header() {
   return (
-    <header className="ui centered">
-      <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
-    </header>
+    <>
+    <Title>
+    <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+    </Title>
+    <Head>
+      <NavLink to={'/'}><Nav>Home</Nav></NavLink>
+      <NavLink to={'/characters/'}><Nav>Characters</Nav></NavLink>
+      <NavLink to={'/locations/'}><Nav>Locations</Nav></NavLink>
+    </Head>
+    </>
   );
 }
